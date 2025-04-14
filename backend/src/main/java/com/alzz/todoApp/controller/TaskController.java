54a51497c8ec745e05a6fb82e7b307d1fa3d820f
Alisha Basa <56277823+alzz-26@users.noreply.github.com>
2025-04-14 21:21:15 +0530

    Update TaskController.java
    updates

--- backend/src/main/java/com/alzz/todoApp/controller/TaskController.java
+++ backend/src/main/java/com/alzz/todoApp/controller/TaskController.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.alzz.todoApp.model.Task;
 import com.alzz.todoApp.repository.TaskRepository;
-
+import java.util.Optional;
 import java.util.List;
 
 @RestController
@@ -27,12 +27,14 @@ public class TaskController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Task> updateTask(@PathVariable Long id) {
-        for (Task task : taskRepo.findAll()) {
-            if (task.getId().equals(id)) {
-                task.setCompleted(!task.isCompleted());
-                return ResponseEntity.ok(task);
-            }
+        Optional<Task> optionalTask = taskRepo.findById(id);
+
+        if (optionalTask.isPresent()) {
+            Task task = optionalTask.get();
+            task.setCompleted(!task.isCompleted());
+            return ResponseEntity.ok(taskRepo.save(task));
         }
+
         return ResponseEntity.notFound().build();
     }
 
